7dd51854c3e76103888169e86aab9e89133846b7
Noah Thorp <noahthorp@rix-10.gateway.2wire.net>
2009-07-05 15:41:03 -0700

    Now updates inversion when moving.

--- hibachi.js
+++ hibachi.js
@@ -1,7 +1,8 @@
 // Jacquard, loom, brocade
 function TriadWeaver() {
-  this.chord = ["maj", 60,64,67]
-  this.inversion = [1,2,3];
+  this.chord_type = "maj";
+  this.chord = [60,64,67];
+  this.inversion = [0,1,2];
 }
 
 function maj(i){
@@ -13,17 +14,15 @@ function min(i){
 }
 
 TriadWeaver.prototype.move = function(direction) {
-  var change = this.MAJ_MAP[direction]
-  this.chord[0] = change[0]
-  
-  this.chord[1] += change[1]
-  this.chord[2] += change[2]
-  this.chord[3] += change[3]
-  this.inversion = change
+  change = this.MAJ_MAP[direction]
+  var note_change = change.slice(1,4); 
+  var inversion_change = change.slice(4,7);
+  this.chord_type = change[0]
+  this.chord = this.weave_chord_change(this.chord, this.inversion, note_change);
+  this.inversion = this.weave_inversion_change(this.inversion,inversion_change);
 }
 
-
-TriadWeaver.prototype.weave_change = function(destination, inversion, change) {
+TriadWeaver.prototype.weave_chord_change = function(destination, inversion, change) {
   for(i = 0; i < inversion.length; i++) {
     //get destination position based on inversion
     mapping = inversion[i]; 
@@ -34,41 +33,51 @@ TriadWeaver.prototype.weave_change = function(destination, inversion, change) {
   return destination;
 }
 
+TriadWeaver.prototype.weave_inversion_change = function(inversion, change) {
+  for(i = 0; i < inversion.length; i++) {
+    //get destination position based on inversion
+    mapping = inversion[i]; 
+
+    //update destination inversion with the inversion at the current index
+    inversion[mapping] = change[i];
+  }
+  return inversion;
+}
 /*
   Map the when starting on a major chord
   Values are:
   Destination chord type
   3 digits which will be added to the root, 3rd and fifth
-  3 digits representing the new positions of the root, third, and fifth
-    this is a relative movement from root position 1,3,5
+  3 digits representing the new "inversion" of the root, third, and fifth
+    as array indexes
 */
 
 TriadWeaver.prototype.MAJ_MAP = [
-  ["min", 0,  0,  2, 2, 3, 1],
-  ["maj", 0,  1,  2, 3, 1, 2],
-  ["min", 0,  1,  1, 3, 1, 2],
-  ["maj", 0,  -1, 1, 2, 3, 1],
-  ["min", 0,  -1, 0, 1, 2, 3],
-  ["maj", -2, -1, 0, 3, 1, 2],
-  ["min", -2, -2, 0, 2, 3, 1],
-  ["maj", -1, -2, 0, 2, 3, 1],
-  ["min", -1, 0,  0, 3, 1, 2],
-  ["maj", -1, 0,  1, 3, 1, 2],
-  ["min", 1,  0,  1, 1, 2, 3],
-  ["maj", 1,  0,  2, 2, 3, 1]
+  ["min", 0,  0,  2, 1, 2, 0],
+  ["maj", 0,  1,  2, 2, 0, 1],
+  ["min", 0,  1,  1, 2, 0, 1],
+  ["maj", 0,  -1, 1, 1, 2, 0],
+  ["min", 0,  -1, 0, 0, 1, 2],
+  ["maj", -2, -1, 0, 2, 0, 1],
+  ["min", -2, -2, 0, 1, 2, 0],
+  ["maj", -1, -2, 0, 1, 2, 0],
+  ["min", -1, 0,  0, 2, 0, 1],
+  ["maj", -1, 0,  1, 2, 0, 1],
+  ["min", 1,  0,  1, 0, 1, 2],
+  ["maj", 1,  0,  2, 1, 2, 0]
   ];
 
 TriadWeaver.prototype.MIN_MAP = [
-  ["maj", 0,  0,  1,  2, 3, 2],
-  ["min", -1, 0,  1,  2, 3, 1],
-  ["maj", -1, 0,  -1, 1, 2, 3],
-  ["min", -2, 0,  -1, 3, 1, 2],
-  ["maj", -2, 0,  0,  3, 1, 2],
-  ["min", -2, -1, 0,  2, 3, 1],
-  ["maj", -1, -1, 0,  2, 3, 1],
-  ["min", -1, 1,  0,  3, 1, 2],
-  ["maj", 0,  1,  0,  1, 2, 3],
-  ["min", 0,  1,  2,  2, 3, 1],
-  ["maj", 0,  2,  2,  3, 1, 2],
-  ["min", 0,  2,  1,  3, 1, 2]
+  ["maj", 0,  0,  1,  1, 2, 0],
+  ["min", -1, 0,  1,  1, 2, 0],
+  ["maj", -1, 0,  -1, 0, 1, 2],
+  ["min", -2, 0,  -1, 2, 0, 1],
+  ["maj", -2, 0,  0,  2, 0, 1],
+  ["min", -2, -1, 0,  1, 2, 0],
+  ["maj", -1, -1, 0,  1, 2, 0],
+  ["min", -1, 1,  0,  2, 0, 1],
+  ["maj", 0,  1,  0,  0, 1, 2],
+  ["min", 0,  1,  2,  1, 2, 0],
+  ["maj", 0,  2,  2,  2, 0, 1],
+  ["min", 0,  2,  1,  2, 0, 1]
   ];
--- spec/models/hibachi_spec.js
+++ spec/models/hibachi_spec.js
@@ -5,23 +5,23 @@ Screw.Unit(function() {
 
     describe('TriadLattice', function () {
       it("describes movements of major chords", function() {
-        expect(lattice.MAJ_MAP[0]).to(equal,["min", 0,  0,  2, 2, 3, 1]);
+        expect(lattice.MAJ_MAP[0]).to(equal,["min", 0,  0,  2, 1, 2, 0]);
       });
 
       it("describes movements of minor chords", function() {
-        expect(lattice.MIN_MAP[0]).to(equal,["maj", 0,  0,  1,  2, 3, 2]);
+        expect(lattice.MIN_MAP[0]).to(equal,["maj", 0,  0,  1,  1, 2, 0]);
       });
 
       it("stores the current note value for each note inversion", function() {
-        expect(lattice.chord).to(equal, ["maj",60,64,67]);
+        expect(lattice.chord).to(equal, [60,64,67]);
       });
 
       it("stores whether the current chord is major or minor", function() {
-        expect(lattice.chord[0]).to(equal, "maj");
+        expect(lattice.chord_type).to(equal, "maj");
       });
 
       it("stores the current chord inversion", function() {
-        expect(lattice.inversion).to(equal, [1,2,3]);
+        expect(lattice.inversion).to(equal, [0,1,2]);
       });
 
       describe("movement", function() {
@@ -30,20 +30,20 @@ Screw.Unit(function() {
         });
 
         it("lattice move updates chord", function() {
-          expect(lattice.chord).to(equal, ["min",60,64,69]);
+          expect(lattice.chord).to(equal, [60,64,69]);
         });
 
         it("updates whether the position is major or minor", function() {
-          expect(lattice.chord[0]).to(equal, "min");
+          expect(lattice.chord_type).to(equal, "min");
         });
 
-        it("redirects the chord changes based on inversion", function() {
-          expect(lattice.weave_change([60,64,69],[1,2,0],[3,2,1])).
+        it("can redirect changes to the chord based on inversion", function() {
+          expect(lattice.weave_chord_change([60,64,69],[1,2,0],[3,2,1])).
             to(equal,[61,67,71]);
         });
 
         it("updates the inversion", function() {
-          expect(lattice.inversion).to(equal, [2,3,1]);
+          expect(lattice.inversion).to(equal, [1,2,0]);
         });
       });
     });
